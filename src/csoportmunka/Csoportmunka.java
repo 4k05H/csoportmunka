c588de402a93d408679c5398b1d493898a24b48f
Szazidayum <szabrinamolnar@gmail.com>
2022-03-07 15:22:55 +0100

    2. és 3. név inicializálása

--- src/csoportmunka/Csoportmunka.java
+++ src/csoportmunka/Csoportmunka.java
@@ -2,6 +2,8 @@ package csoportmunka;
 public class Csoportmunka {
     public static void main(String[] args) {
         String nev1="Szabrina";
+        String nev2;
+        String nev3;
    
     }
     
